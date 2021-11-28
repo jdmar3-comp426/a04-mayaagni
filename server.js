690c0ae125cdd0288022507b4aa9a9ef2e9b8f24
Maya Agnihotri <mayaagni@live.unc.edu>
2021-11-28 12:54:56 -0500

    new

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.patch("/app/update/user/:id", (req, res) => {
 app.delete("/app/delete/user/:id", (req, res) => {	
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?")
 	const info = stmt.run(req.params.id);
-	res.json({"message": info.changes + " record updated: ID " + req.params.id + " (200)"});
+	res.json({"message": info.changes + " record deleted: ID " + req.params.id + " (200)"});
 });
 // Default response for any other request
 app.use(function(req, res){
